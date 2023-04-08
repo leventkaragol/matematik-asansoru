e3263233c86f1d041aeb01675a91018f37b34f9e
H.Levent KARAGÖL <mail@leventkaragol.com>
2023-04-08 17:41:49 +0300

    Temel bileşenler eklendi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { GameComponent } from './components/game/game.component';
+import { NewGameComponent } from './components/shared/new-game/new-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashScreenComponent,
+    MainMenuComponent,
+    GameComponent,
+    NewGameComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/shared/new-game/new-game.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-game',
+  templateUrl: './new-game.component.html',
+  styleUrls: ['./new-game.component.css']
+})
+export class NewGameComponent {
+
+}
